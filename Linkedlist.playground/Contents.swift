f57c46f2f4b708644752353d2be95140cf9a5a5f
Karim <KariimAhmed8@gmail.com>
2020-12-06 12:10:28 +0200

    # Adding pop method

--- Linkedlist.playground/Contents.swift
+++ Linkedlist.playground/Contents.swift
@@ -13,14 +13,14 @@ struct LinkedList<Value> {
     }
     
     init() {}
-    
+    // add new element in the Head of the List
     mutating func push (value:Value) {
         head = Node(value: value, next: head)
         if tail == nil {
             tail = head
         }
     }
-    
+    // add new element in the Tail of the List
     mutating func append(value: Value) {
         
         guard !isEmpty else {
@@ -33,6 +33,40 @@ struct LinkedList<Value> {
         tail!.next = node
         tail = node
     }
+    
+    // add new element in Particular postion of the List
+    //This operation required two stepse 1st get Node    2nd insert after
+    func getNode(at index:Int) -> Node<Value>? {
+        var currentIndex = 0
+        var currentNode = head
+        
+        while(currentNode != nil && currentIndex < index) { // value will be returned once  == the index paramter
+            currentIndex += 1
+            currentNode = currentNode?.next
+        }
+        return currentNode
+    }
+    
+    func insert (value:Value, after node:Node<Value>) {
+        node.next = Node(value: value, next: node.next)
+    }
+    
+    
+    
+    // Pop element from the List
+    mutating func pop () -> Value? {
+        //Here in each tim i'll call pop method i will change the head to be the next node
+        defer {
+            head = head?.next
+            if isEmpty {
+                tail = nil
+            }
+        }
+        return head?.value
+    }
+
+
+
 }
 
 
@@ -80,9 +114,16 @@ var list = LinkedList<Int>()
 //list.push(value: 60)
 
 //Append
-list.append(value: 0)
-list.append(value: 0)
-list.append(value: 0)
+list.append(value: 10)
+list.append(value: 20)
+list.append(value: 30)
+print(list)
+
+//let node = list.getNode(at: 1)! // get required node
+//list.insert(value: 999, after: node) // append value after
+
+list.pop()
+list.pop()
 
 
 print(list)
