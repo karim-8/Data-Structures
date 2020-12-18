550406defe39fdffc6bb751cc6c4978678be8000
Karim <KariimAhmed8@gmail.com>
2020-12-18 15:21:06 +0200

    #Adding comments

--- Buble Sort.playground/Contents.swift
+++ Buble Sort.playground/Contents.swift
@@ -2,17 +2,17 @@ import UIKit
 
 var numbers = [10,60,50,7,8,99,256,88,20,71]
 
-//temp =10
-  //60 , 10 , 50 ...
-   // temp = 60
+// i == 60
+// j == 10
 for i in 0..<numbers.count {
     for j in 0..<numbers.count {
         
-        if numbers[i] < numbers[j] {
+        if numbers[i] > numbers[j] {// 10 > 60 - 10 > 50 - 10 > 7  ----->
             
-            let temp = numbers[i]
-            numbers[i] = numbers[j]
-            numbers[j] = temp
+            let temp = numbers[i]    //temp = 10
+            numbers[i] = numbers[j] // numbers[i] == 7
+            numbers[j] = temp       // numbers[j] == 10
+            // we are appending elements so the last bigger will be the first element
         }
     }
 }
