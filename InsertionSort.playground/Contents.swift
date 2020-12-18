26b62c5ac5302b4c87122e94270c486aa1808fff
Karim <KariimAhmed8@gmail.com>
2020-12-18 17:21:21 +0200

    #comments

--- InsertionSort.playground/Contents.swift
+++ InsertionSort.playground/Contents.swift
@@ -1,15 +1,16 @@
 import UIKit
 
 
-var numbers = [10,60,50,7,8,99,256,88,20,71]
+var numbers = [100,60,50,7,8,99,256,88,20,71]
 
 for i in 0..<numbers.count {
-    let key = numbers[i]
-    var j = i - 1
     
-    while j >= 0 && numbers[j] > key {
-        numbers[j+1] = numbers[j]
-        j = j - 1
+    let key = numbers[i] //60
+    var j = i - 1 //100
+    
+    while j >= 0 && numbers[j] > key {  //0 && 100 > 60
+        numbers[j+1] = numbers[j]   //60 = 100
+        j = j - 1   
     }
     numbers[j + 1] = key
 }
