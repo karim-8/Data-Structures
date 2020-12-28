3672f3e0652a8242051a1d0f87e3724c0614a7cb
Karim <KariimAhmed8@gmail.com>
2020-12-28 17:29:06 +0200

    #Adding Vertex

--- Graph.playground/Contents.swift
+++ Graph.playground/Contents.swift
@@ -94,3 +94,49 @@ class AdjacencyList<T: Hashable> :Graph {
     }
     
 }
+
+extension AdjacencyList: CustomStringConvertible {
+    public var description: String {
+        var result = ""
+        for (vertex, edges) in adjancencies { // 1
+            var edgeString = ""
+            for (index, edge) in edges.enumerated() { // 2
+                if index != edges.count - 1 {
+                    edgeString.append("\(edge.destination), ")
+                } else {
+                    edgeString.append("\(edge.destination)")
+                }
+            }
+            result.append("\(vertex) ---> [ \(edgeString) ]\n") // 3
+        }
+        return result
+    }
+    
+}
+
+let graph = AdjacencyList<String>()
+
+let singapore = graph.createVertex(data: "Singapore")
+let tokyo = graph.createVertex(data: "Tokyo")
+let hongKong = graph.createVertex(data: "Hong Kong")
+let detroit = graph.createVertex(data: "Detroit")
+let sanFrancisco = graph.createVertex(data: "San Francisco")
+let washingtonDC = graph.createVertex(data: "Washington DC")
+let austinTexas = graph.createVertex(data: "Austin Texas")
+let seattle = graph.createVertex(data: "Seattle")
+
+graph.add(.undirected, from: singapore, to: hongKong, weight: 300)
+graph.add(.undirected, from: singapore, to: tokyo, weight: 500)
+graph.add(.undirected, from: hongKong, to: tokyo, weight: 250)
+graph.add(.undirected, from: tokyo, to: detroit, weight: 450)
+graph.add(.undirected, from: tokyo, to: washingtonDC, weight: 300)
+graph.add(.undirected, from: hongKong, to: sanFrancisco, weight: 600)
+graph.add(.undirected, from: detroit, to: austinTexas, weight: 50)
+graph.add(.undirected, from: austinTexas, to: washingtonDC, weight: 292)
+graph.add(.undirected, from: sanFrancisco, to: washingtonDC, weight: 337)
+graph.add(.undirected, from: washingtonDC, to: seattle, weight: 277)
+graph.add(.undirected, from: sanFrancisco, to: seattle, weight: 218)
+graph.add(.undirected, from: austinTexas, to: sanFrancisco, weight: 297)
+
+print(graph)
+
